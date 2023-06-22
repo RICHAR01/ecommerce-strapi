6407ffc1713d45a7f08d127d5671c68eb6f50395
Ricardo Rocha <ricardoproblaz@gmail.com>
2023-06-22 15:27:20 -0700

    first commit

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const stripe = require("stripe")(
+  "sk_test_51MlOAiEc6pjAeJ95oS28A6CbA4e71iXpWachHwHz7vltUFkCXHXAgcT07gAi7ew1eMjecbeltuwZTFjCtFxawzEC00fjXZbE2I"
+);
+
+function calcDiscountPrice(price, discount) {
+  if (!discount) return price;
+
+  const discountAmount = (price * discount) / 100;
+  const result = price - discountAmount;
+
+  return result.toFixed(2);
+}
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async paymentOrder(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+
+    console.log("addressShipping: ", addressShipping);
+    console.log("products: ", products);
+
+    let totalPayment = 0;
+    products.forEach((product) => {
+      const priceTemp = calcDiscountPrice(
+        product.attributes.price,
+        product.attributes.discount
+      );
+
+      totalPayment += Number(priceTemp) * product.quantity;
+    });
+
+    console.log("star charge");
+
+    const charge = await stripe.charges.create({
+      amount: Math.round(totalPayment * 100),
+      currency: "eur",
+      source: token.id,
+      description: `User ID: ${idUser}`,
+    });
+
+    const data = {
+      products,
+      user: idUser,
+      totalPayment,
+      idPayment: charge.id,
+      addressShipping,
+    };
+
+    const model = strapi.contentTypes["api::order.order"];
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      model,
+      data
+    );
+
+    const entry = await strapi.db
+      .query("api::order.order")
+      .create({ data: validData });
+
+    return entry;
+  },
+}));
